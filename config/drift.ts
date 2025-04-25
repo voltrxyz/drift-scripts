4e1cecc09284eeabb5ccc868dfc1e131b391c3ba
jakeyvee <jakeyveee@gmail.com>
2025-04-26 01:54:10 +0800

    add drift user features

---
+++ config/drift.ts
@@ -0,0 +1,7 @@
+import { DRIFT } from "../src/constants/drift";
+
+export const depositStrategyAmount = 1_000_000;
+export const withdrawStrategyAmount = 1_000_000;
+
+// DRIFT VARIABLES: ONLY MAIN MARKET SUPPORTED, CHANGE AND ALIGN TO ASSET TOKEN
+export const driftMarketIndex = DRIFT.SPOT.USDC.MARKET_INDEX;
---
+++ src/scripts/manager-init-user.ts
@@ -0,0 +1,149 @@
+import "dotenv/config";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SYSVAR_RENT_PUBKEY,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import {
+  sendAndConfirmOptimisedTx,
+  setupAddressLookupTable,
+  setupTokenAccount,
+} from "../utils/helper";
+import { BN } from "@coral-xyz/anchor";
+import * as fs from "fs";
+import { VoltrClient } from "@voltr/vault-sdk";
+import {
+  assetMintAddress,
+  assetTokenProgram,
+  lookupTableAddress,
+  useLookupTable,
+  vaultAddress,
+} from "../../config/base";
+import { ADAPTOR_PROGRAM_ID, DISCRIMINATOR, DRIFT } from "../constants/drift";
+
+const payerKpFile = fs.readFileSync(process.env.MANAGER_FILE_PATH!, "utf-8");
+const payerKpData = JSON.parse(payerKpFile);
+const payerSecret = Uint8Array.from(payerKpData);
+const payerKp = Keypair.fromSecretKey(payerSecret);
+const payer = payerKp.publicKey;
+
+const vault = new PublicKey(vaultAddress);
+const vaultAssetMint = new PublicKey(assetMintAddress);
+const vaultAssetTokenProgram = new PublicKey(assetTokenProgram);
+
+const connection = new Connection(process.env.HELIUS_RPC_URL!);
+const vc = new VoltrClient(connection);
+
+const initDriftUser = async (
+  protocolProgram: PublicKey,
+  state: PublicKey,
+  subAccountId: BN
+) => {
+  const [strategy] = PublicKey.findProgramAddressSync(
+    [Buffer.from("drift_user")],
+    new PublicKey(ADAPTOR_PROGRAM_ID)
+  );
+
+  const { vaultStrategyAuth } = vc.findVaultStrategyAddresses(vault, strategy);
+  const [userStats] = PublicKey.findProgramAddressSync(
+    [Buffer.from("user_stats"), vaultStrategyAuth.toBuffer()],
+    protocolProgram
+  );
+
+  const [user] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("user"),
+      vaultStrategyAuth.toBuffer(),
+      subAccountId.toArrayLike(Buffer, "le", 2),
+    ],
+    protocolProgram
+  );
+
+  let transactionIxs: TransactionInstruction[] = [];
+
+  const vaultStrategyAssetAta = await setupTokenAccount(
+    connection,
+    payer,
+    vaultAssetMint,
+    vaultStrategyAuth,
+    transactionIxs,
+    vaultAssetTokenProgram
+  );
+
+  const vaultName = await vc
+    .fetchVaultAccount(vault)
+    .then((vault) => vault.name);
+
+  console.log("Vault name:", vaultName);
+
+  const createInitializeStrategyIx = await vc.createInitializeStrategyIx(
+    {
+      instructionDiscriminator: Buffer.from(DISCRIMINATOR.INITIALIZE_USER),
+      additionalArgs: Buffer.from(vaultName),
+    },
+    {
+      payer,
+      vault,
+      manager: payer,
+      strategy,
+      remainingAccounts: [
+        { pubkey: protocolProgram, isSigner: false, isWritable: false },
+        { pubkey: userStats, isSigner: false, isWritable: true },
+        { pubkey: user, isSigner: false, isWritable: true },
+        { pubkey: state, isSigner: false, isWritable: true },
+        { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+      ],
+      adaptorProgram: new PublicKey(ADAPTOR_PROGRAM_ID),
+    }
+  );
+
+  transactionIxs.push(createInitializeStrategyIx);
+
+  const txSig = await sendAndConfirmOptimisedTx(
+    transactionIxs,
+    process.env.HELIUS_RPC_URL!,
+    payerKp
+  );
+  console.log("Drift strategy initialized with signature:", txSig);
+
+  if (useLookupTable) {
+    const transactionIxs1: TransactionInstruction[] = [];
+
+    await setupAddressLookupTable(
+      connection,
+      payer,
+      payer,
+      [
+        ...new Set([
+          ...createInitializeStrategyIx.keys.map((k) => k.pubkey.toBase58()),
+          vaultStrategyAssetAta.toBase58(),
+        ]),
+      ],
+      transactionIxs1,
+      new PublicKey(lookupTableAddress)
+    );
+
+    const txSig1 = await sendAndConfirmOptimisedTx(
+      transactionIxs1,
+      process.env.HELIUS_RPC_URL!,
+      payerKp,
+      [],
+      undefined,
+      50_000
+    );
+
+    console.log("LUT updated with signature:", txSig1);
+  }
+};
+
+const main = async () => {
+  await initDriftUser(
+    new PublicKey(DRIFT.PROGRAM_ID),
+    new PublicKey(DRIFT.SPOT.STATE),
+    new BN(DRIFT.SUB_ACCOUNT_ID)
+  );
+};
+
+main();
